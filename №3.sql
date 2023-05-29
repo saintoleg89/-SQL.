321662737c165954f4de5d2419f29de4b6f28d1f
saintoleg89 <119620553+saintoleg89@users.noreply.github.com>
2023-05-29 13:18:03 +0300

    Add files via upload

--- №3.sql
+++ №3.sql
@@ -9,7 +9,7 @@ limit 1;
 
 /*название треков, продолжительность которых не менее 3,5 минуты;*/
 SELECT name  FROM track
-where duration >= 3.5*60;
+where duration >= 3.5;
 
 /*названия сборников, вышедших в период с 2018 по 2020 год включительно;*/
 SELECT name, year_of_release  FROM collection
@@ -20,5 +20,17 @@ SELECT name  FROM musician
 where name not like '% %';
 
 /*название треков, которые содержат слово "мой"/"my".*/
+SELECT name FROM track /* Имя трека из таблицы треков */
+WHERE LOWER(name) ILIKE 'my %' /* Где слово в начале строки */
+OR LOWER(name) ILIKE '% my' /* Где слово в конце строки */
+OR LOWER(name) ILIKE '% my %' /* Где слово в середине строки */
+OR LOWER(name) ILIKE 'my'; 
+
 SELECT name FROM track
-WHERE LOWER(name) like '%my%' or LOWER(name) like '%мой%';
+WHERE LOWER(name) ILIKE 'мой %' /* Где слово в начале строки */
+OR LOWER(name) ILIKE '% мой' /* Где слово в конце строки */
+OR LOWER(name) ILIKE '% мой %' /* Где слово в середине строки */
+OR LOWER(name) ILIKE 'мой';
+
+SELECT name FROM track 
+WHERE string_to_array ( 'Wishmaster',  ' ');
